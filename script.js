43d6ffb78a44125e5758b162eb1b0d2ecd078fd5
Nancy Taneva <nancy.taneva@gmail.com>
2023-06-13 16:54:27 +0300

    1

--- script.js
+++ script.js
@@ -3,16 +3,18 @@ console.log(clienBirthYear);
 var clientAge = (2023 - clienBirthYear);
 
         if(clienBirthYear < 1965) {
-            var firstDigitRandomNumber  = Math.ceil(Math.random()*(7 - 1) + 1);
-            var firstDigit              = Math.ceil(clientAge / firstDigitRandomNumber);
-            
+            while(true) {
+                var firstDigitRandomNumber  = Math.ceil(Math.random()*(7 - 1) + 1);
+                var firstDigit              = Math.ceil(clientAge / firstDigitRandomNumber);
+                
                 if(firstDigit > 9) {
                     firstDigit = parseInt(firstDigit / 10);
+                }   
+                if(firstDigit % 2 == 1) {
+                console.log(`First digit: ${firstDigit}`);
+                break;
                 }
-            if(firstDigit % 2 == 1) {
-            console.log(`First digit: ${firstDigit}`);
             }
-
         }
 
         if(clienBirthYear > 1965) {
