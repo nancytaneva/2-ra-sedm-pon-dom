ba317c0c71c215ecf7fcf7f4f3c034236155586a
Nancy Taneva <nancy.taneva@gmail.com>
2023-06-13 15:54:45 +0300

    1

--- script.js
+++ script.js
@@ -22,32 +22,28 @@
 
 var clienBirthYear = prompt("През коя година си роден?");
 console.log(clienBirthYear);
+const clientAge = 2023 - clienBirthYear;
 
-// ако е роден преди 1965:
-if(clienBirthYear < 1965) {
+    while(clienBirthYear != 1965) {
 
-        
-        const clientAge = 2023 - clienBirthYear;
-        var firstDigitRandomNumber = Math.ceil(Math.random()*(7 - 1) + 1);
-        var firstDigit = parseInt(Math.ceil(clientAge / firstDigitRandomNumber));
-    while(firstDigit) {
-            // if(firstDigit % 2 == 0) {
-            // }
+        if(clienBirthYear < 1965) {
+            var firstDigitRandomNumber = Math.ceil(Math.random()*(7 - 1) + 1);
+            var firstDigit = Math.ceil(clientAge / firstDigitRandomNumber);
+            
+                if(firstDigit > 9) {
+                    firstDigit = parseInt(firstDigit / 10);
+                }
             if(firstDigit % 2 == 1) {
             console.log(`First digit: ${firstDigit}`);
-            break;
             }
+
+        }
+
+        if(clienBirthYear > 1965) {
+            console.log("Nad 1965");
+        }
     }
+
+    firstDigit == 0;
+    console.log(`First digit: ${firstDigit}`);
     
-}
-
-// ако е роден след 1965:
-// if(clienBirthYear > 1965) {
-//     firstDigit % 2 == 1;
-// }
-
-// // ако е роден през 1965:
-// if(firstDigit == 1965) {
-//  firstDigit == 0;
-//  console.log(`First digit: ${firstDigit}`);
-// }
