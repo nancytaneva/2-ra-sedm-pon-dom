afb49a3332b113bf3c6f8b015726e27c2bb85d3a
Nancy Taneva <nancy.taneva@gmail.com>
2023-06-13 17:54:50 +0300

    fixed

--- script.js
+++ script.js
@@ -187,23 +187,23 @@ if(clientBMI >= 40) {
     // 3 - често
 // Цифрата е сумата от позицията на избрания продукт и честотата на пазаруване
 // Пример: Избрали сме позиция 3. Цигари и алкохол - честота понякога - резултата е 3 + 2
-const clientFavProducts               = prompt(`Кой вид продукти най-много обичаш да консумираш? Отговори с ЦИФРА: 1 = плодове и зеленчуци; 2 = месо и месни продукти; 3 = цигари и алкохол; 4 = млечни продукти; 5 = захарни изделия`);
+var clientFavProducts               = prompt(`Кой вид продукти най-много обичаш да консумираш? Отговори с ЦИФРА: 1 = плодове и зеленчуци; 2 = месо и месни продукти; 3 = цигари и алкохол; 4 = млечни продукти; 5 = захарни изделия`);
 
 
 if(clientFavProducts == 1) {
-    const clientFavProductFrequency = prompt(`Колко често купувате плодове и зеленчуци? Отговори чрез "1" рядко, "2" понякога и "3" често`);
+    var clientFavProductFrequency = prompt(`Колко често купувате плодове и зеленчуци? Отговори чрез "1" рядко, "2" понякога и "3" често`);
 }
 else if(clientFavProducts == 2) {
-    const clientFavProductFrequency = prompt(`Колко често купувате месо и месни продукти? Отговори чрез "1" рядко, "2" понякога и "3" често`);
+    var clientFavProductFrequency = prompt(`Колко често купувате месо и месни продукти? Отговори чрез "1" рядко, "2" понякога и "3" често`);
 }
 else if(clientFavProducts == 3) {
-    const clientFavProductFrequency = prompt(`Колко често купувате цигари и алкохол? Отговори чрез "1" рядко, "2" понякога и "3" често`);
+    var clientFavProductFrequency = prompt(`Колко често купувате цигари и алкохол? Отговори чрез "1" рядко, "2" понякога и "3" често`);
 }
 else if(clientFavProducts == 4) {
-    const clientFavProductFrequency = prompt(`Колко често купувате млечни продукти? Отговори чрез "1" рядко, "2" понякога и "3" често`);
+    var clientFavProductFrequency = prompt(`Колко често купувате млечни продукти? Отговори чрез "1" рядко, "2" понякога и "3" често`);
 }
 else if(clientFavProducts == 5) {
-    const clientFavProductFrequency = prompt(`Колко често купувате захарни изделия? Отговори чрез "1" рядко, "2" понякога и "3" често`);
+    var clientFavProductFrequency = prompt(`Колко често купувате захарни изделия? Отговори чрез "1" рядко, "2" понякога и "3" често`);
 }
 
 const fifthDigit = parseInt(clientFavProducts) + parseInt(clientFavProductFrequency);
